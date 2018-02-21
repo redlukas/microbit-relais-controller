4e2e78d3e439a6a771e1096795978cd56de8cde0
redlukas <redlukas@users.noreply.github.com>
2018-02-21 17:26:25 +0100

    debugging on a microbit sucks

--- control.py
+++ control.py
@@ -4,28 +4,35 @@ counter = 0
 
 scnCount = 3
 
-def incrementer():
-	if button_a.is_pressed()
-		counter = counter + 1
-
-incrementer()
 
 
-if counter % scnCount == 0:
+def off():
 	pin2.write_digital(0)
 	display.scroll("off")
+	counter = button_a.get_presses + counter
 	
-elif counter % scnCount == 1:
+def on():
 	pin2.write_digital(1)
 	display.scroll("on")
+	counter = button_a.get_presses + counter
 
-elif counter % scnCount == 2:
+def blink():
 	pin2.write_digital(1)
 	sleep(500)
 	pin2.write_digital(0)
 	sleep(500)
 	display.scroll("blink")
+	counter = button_a.get_presses + counter
+	
+while true:
+	if counter % scnCount == 0:
+	off()
+	
+	elif counter % scnCount == 1:
+		on()
 
-else:
-	display.scroll("?")
+	elif counter % scnCount == 2:
+		blink()
 
+	else:
+		display.scroll("ka")
