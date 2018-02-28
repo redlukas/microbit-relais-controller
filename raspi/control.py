a0d6c3abf2ef350e2f1c972c8d7d3e85e5000fa4
redlukas <redlukas@users.noreply.github.com>
2018-02-28 17:59:38 +0100

    works even better

--- raspi/control.py
+++ raspi/control.py
@@ -17,35 +17,56 @@ GPIO.setup(inPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 ###functions for the different scenes
 def off():
 	GPIO.output(outPin,GPIO.LOW)
-	print "0"
+	print "0" + "\r",
 
 def on():
 	GPIO.output(outPin,GPIO.HIGH)
-	print "1"
+	print "1" + "\r",
 
 def blink():
-	print "B"
-	GPIO.output(outPin,GPIO.HIGH)
-	time.sleep(0.5)
+	print "b" + "\r",
 	GPIO.output(outPin,GPIO.LOW)
 	time.sleep(0.5)
+	GPIO.output(outPin,GPIO.HIGH)
+	time.sleep(0.5)
+
+def fastblink():
+	print "B" + "\r",
+	GPIO.output(outPin,GPIO.LOW)
+	time.sleep(0.5)
+	GPIO.output(outPin,GPIO.HIGH)
+	time.sleep(0.1)
+	GPIO.output(outPin,GPIO.LOW)
+	time.sleep(0.5)
+	GPIO.output(outPin,GPIO.HIGH)
+	time.sleep(0.1)
+	GPIO.output(outPin,GPIO.LOW)
+	time.sleep(0.5)
+	GPIO.output(outPin,GPIO.HIGH)
+	time.sleep(0.1)
+	GPIO.output(outPin,GPIO.LOW)
+	time.sleep(0.5)
+	GPIO.output(outPin,GPIO.HIGH)
+	time.sleep(0.1)
 
 ###list containing the scenes, remember to add your scenes here
-scenes = {0 : off, 1 : on, 2 : blink}
+scenes = {0 : off, 1 : on, 2 : blink, 3 : fastblink}
+
+
 
 while True:
-	if counter > 2:
+	if counter >= len(scenes):
         	counter = 0
 	scenes[counter]()
 	now = counter
 	while now == counter:
+		scenes[counter]()
+		print "waiting" + "\r",
 		input_state = GPIO.input(inPin)
 		if input_state == True:
 			counter = counter + 1
 			time.sleep(1)
-			print "waiting"
-			time.sleep(2)
-		else:
+			print " "
 			break
 
    
