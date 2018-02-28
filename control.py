c40249615597e133f9a7c41922bb2c18a7e71ec0
redlukas <redlukas@users.noreply.github.com>
2018-02-28 09:03:07 +0100

    try this

--- control.py
+++ control.py
@@ -4,32 +4,34 @@ counter = button_a.get_presses
 scnCount = 3
 
 def off():
+	global counter
 	pin2.write_digital(0)
 	display.scroll("0")
 	counter = counter + button_a.get_presses
-	
+
 def on():
+	global counter
 	pin2.write_digital(1)
 	display.scroll("1")
 	counter = counter + button_a.get_presses
 
 def blink():
+	global counter
 	pin2.write_digital(1)
 	sleep(500)
 	pin2.write_digital(0)
 	sleep(500)
 	display.scroll("B")
 	counter = counter + button_a.get_presses
-	
-while True:
-	if counter == 0:
-		off()
-	
-	elif counter == 1:
-		on()
 
-	elif counter == 2:
-		blink()
+while counter == 0:
+	off()
 
-	else:
-		display.scroll("k")
+while counter == 1:
+	on()
+
+while counter == 2:
+	blink()
+
+if counter > 2:
+	counter = 0
