50a9d7f796805918d146ad5ff8d3d63b4c112843
redlukas <redlukas@users.noreply.github.com>
2018-03-04 15:49:58 +0100

    PWM scene added

--- standalone/controller.py
+++ standalone/controller.py
@@ -7,40 +7,47 @@ outPin=2
 
 
 ###functions for the different scenes
-def off():
+def off(i):
 	pin2.write_digital(0)
 	display.show("0")
 
-def on():
+def on(i):
 	pin2.write_digital(1)
 	display.show("1")
 
-def blink():
+def blink(i):
 	display.show("b")
 	pin2.write_digital(0)
 	sleep(500)
 	pin2.write_digital(1)
 	sleep(500)
 
-def fastblink():
+def fastblink(i):
 	display.show("B")
 	pin2.write_digital(0)
-	sleep(500)
+	sleep(900)
 	pin2.write_digital(1)
-	sleep(1000)
+	sleep(150)
+
+def fadeinout(i):
+    display.show("f")
+    for i in range (0, 1023):
+        pin2.write_analog(i)
+    for i in range (1023, 0):
+        pin2.write_analog(i)
 
 ###list containing the scenes, remember to add your scenes here
-scenes = {0 : off, 1 : on, 2 : blink, 3 : fastblink}
+scenes = {0 : off, 1 : on, 2 : blink, 3 : fastblink, 4 : fadeinout}
 
 
 
 while True:
 	if counter >= len(scenes):				#reset the counter if all scenes have been played
         	counter = 0
-	scenes[counter]()					#rocknroll
+	scenes[counter](i)					#rocknroll
 	now = counter						#so we know what we are playing
 	while now == counter:			
-		scenes[counter]()			
+		scenes[counter](i)			
 		if button_a.was_pressed() == True:		#listen for the press of a button
 			counter = counter + 1			#increment the counter on buttonpress
 			break
